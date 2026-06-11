f9af8f7b60ac080a3b27fcddcf34f975470c750b
Omkar Nalawade <nalawadeomkar2302@gmail.com>
2026-06-11 10:24:20 +0530

    Created project structure and screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'screens/splash_screen.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(body: Center(child: Text("Welcome to CareConnect"))),
-    ),
-  );
+  runApp(const CareConnectApp());
+}
+
+class CareConnectApp extends StatelessWidget {
+  const CareConnectApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
 }
